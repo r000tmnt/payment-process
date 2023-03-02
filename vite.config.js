a4d917abeb49aa6193a011a614f122fb312f21ab
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2023-03-02 14:00:39 +0800

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/payment-process',
+  base: './payment-process',
   plugins: [vue()],
   resolve: {
     alias: {

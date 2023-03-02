a946c8c37855af86834948fa0f24eef37e4f2834
r000tmnt <62630285+r000tmnt@users.noreply.github.com>
2023-03-02 16:43:28 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/payment-process/',
+  base: '/payment_process/',
   plugins: [vue()],
   resolve: {
     alias: {

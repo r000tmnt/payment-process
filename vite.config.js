e60277d94e2ef78abe19d93f1a7f6c5520398729
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2023-08-11 16:37:01 +0800

    [TEST] build mode action

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/payment_process/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  preprocessorOptions: {
-    scss: {
-      additionalData: `@import "./src/assets/global.scss";`
-    }
+export default defineConfig(({mode}) => {
+  const config = {
+    base: '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/assets/global.scss";`
+      }
+    }    
   }
+
+  if(mode === 'action'){
+    config.base = '/payment_process/'
+  }
+
 })
